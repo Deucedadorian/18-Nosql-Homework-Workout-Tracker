efac62443440d0f9674ce3718e5e76762c74dae5
Dylan Cole <logboarddjc@gmail.com>
2021-07-09 12:12:52 -0400

    add workout model and server.js

---
+++ models/workout.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: {
+        type: Array,
+        required: true,
+    }
+});
