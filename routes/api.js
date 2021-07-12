2831b920611e31c3d55dd88ca0f480f2d92f2422
Dylan Cole <logboarddjc@gmail.com>
2021-07-12 16:48:48 -0400

    change routes/api.js so a new exercise is pushed to the array of exercises correctly. duration is now calculated on homepage

--- routes/api.js
+++ routes/api.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.put("/api/workouts/:id", (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     Workout.updateOne({ "_id": req.params.id }, {"$push": {exercises: req.body}})
     .then((dbWorkout) => {
         res.json(dbWorkout)
     })
     .catch(err => {
         res.status(400).json(err);
-      });
+    });
 });
 
 router.get("/api/workouts", (req, res) => {
@@ -19,7 +19,7 @@ router.get("/api/workouts", (req, res) => {
     })
     .catch(err => {
         res.status(400).json(err);
-      });
+    });
 });
 
 router.post("/api/workouts", (req, res) => {
@@ -27,9 +27,24 @@ router.post("/api/workouts", (req, res) => {
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
+});
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find(
+        {
+            day: 
+            {
+                $gte: new Date((new Date().getTime() - (7 * 24 * 60 * 60 * 1000)))
+            }
+        }
+        ).sort({ "date": -1 }
+    )
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
     .catch(err => {
         res.status(400).json(err);
     });
-});
+})
 
 module.exports = router;
